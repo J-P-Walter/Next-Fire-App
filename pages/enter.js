c063a7567fd740c8940eb476ad43f4f804c6546a
JP <j.p.walter@outlook.com>
2022-09-22 15:58:33 -0400

    Working on authentication with Google. Needed to change code due to update

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,40 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
+
 export default function Enter({}) {
+  const user = true;
+  const username = true;
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+//Sign in with Google
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+//Sign out of google
+function SignOutButton() {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
+}
+
+function UsernameForm() {}
