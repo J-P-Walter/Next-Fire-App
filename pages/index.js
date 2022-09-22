781fed95ec251dc8923afde4a6f7b86300775a4e
JP <j.p.walter@outlook.com>
2022-09-22 13:06:32 -0400

    Learned and implemented dynamic routing using []

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/Link";
 
 export default function Home() {
-  return <div>p</div>;
+  return (
+    <div>
+      <Link href="edit">
+        <a>Here is a link</a>
+      </Link>
+    </div>
+  );
 }
