c96da66dbb03c83ad66d86a90a76f1f41c9e0e0a
JP <j.p.walter@outlook.com>
2022-09-27 15:35:14 -0400

    Forgot to include these in prev push

---
+++ components/UserProfile.js
@@ -0,0 +1,13 @@
+//UI componenet for user imgage, username, and profile name
+
+export default function UserProfile({ user }) {
+  return (
+    <div className="box-center">
+      <img src={user.photoURL} className="card-img-center" />
+      <p>
+        <i>@{user.username}</i>
+      </p>
+      <h1>{user.displayName}</h1>
+    </div>
+  );
+}
