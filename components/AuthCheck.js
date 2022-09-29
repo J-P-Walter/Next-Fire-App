293167912e7b7581c318c14dca40672ceeae77ae
JP <j.p.walter@outlook.com>
2022-09-29 12:08:51 -0400

    Added auth check for admin page where users can manage their posts

---
+++ components/AuthCheck.js
@@ -0,0 +1,13 @@
+import Link from "next/Link";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
+//Component's children only shown to logged in users
+export default function AuthCheck(props) {
+  const { username } = useContext(UserContext);
+
+  //If signed in, renders the children, if not, renders fallback or link to sign in page
+  return username
+    ? props.children
+    : props.fallback || <Link href="/enter">You must be signed in</Link>;
+}
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,13 @@
+import AuthCheck from "../../components/AuthCheck";
+
+//Client rendering only since these pages don't need
+//to be seen by webcrawlers
 export default function AdminPostPage({}) {
-  return <main>Admin post page</main>;
+  return (
+    <main>
+      <AuthCheck>
+        <h1>Admin post page</h1>
+      </AuthCheck>
+    </main>
+  );
 }
